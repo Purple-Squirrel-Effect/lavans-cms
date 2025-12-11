fe9b4beaa57a49584ccc3497848275b744022a98
pieter-experience-republic <pieter@experiencerepublic.nl>
2025-12-11 10:43:25 +0100

    Switched out adapter from cf to vercel

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,5 +1,6 @@
 import PocketBase from 'pocketbase';
+import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
 import type { TypedPocketBase } from '../../pocketbase-types';
 
-export const pb = new PocketBase('http://127.0.0.1:8090') as TypedPocketBase;
+export const pb = new PocketBase(PUBLIC_POCKETBASE_URL || 'http://127.0.0.1:8090') as TypedPocketBase;
 
